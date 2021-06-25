e27c7e73d975adf8ed23e4de7a4b2809b1fdd5f1
D <me@kumo.dev>
2021-06-25 23:49:28 +1000

    add login errors

--- src/users.ts
+++ src/users.ts
@@ -1,15 +1,26 @@
 import { apiError, apiSuccess } from './api';
 import { extend, optional, rec, Reify, str, union } from './serialization';
 
+/* Login */
+export type LoginRequest = Reify<typeof serializeLoginRequest>;
+export const [serializeLoginRequest, deserializeLoginRequest] = rec('loginRequest', {
+  username: str('username'),
+  password: str('password'),
+});
+export type LoginError = Reify<typeof loginError>;
+const loginError = extend('loginError', apiError, {});
+export type LoginResponse = Reify<typeof serializeLoginResponse>;
+export const [serializeLoginResponse, deserializeLoginResponse] = union('loginResponse', 'success', [apiSuccess, loginError]);
+
+/* Signup */
 export type SignupRequest = Reify<typeof serializeSignupRequest>;
 export const [serializeSignupRequest, deserializeSignupRequest] = rec('signupRequest', {
   username: str('username'),
   email: str('email'),
   password: str('password'),
 });
-
 export type SignupError = Reify<typeof signupError>;
-export const signupError = extend('signupError', apiError, {
+const signupError = extend('signupError', apiError, {
   username: optional(str('username')),
   email: optional(str('email')),
   password: optional(str('password')),
@@ -17,6 +28,7 @@ export const signupError = extend('signupError', apiError, {
 export type SignupResponse = Reify<typeof serializeSignupResponse>;
 export const [serializeSignupResponse, deserializeSignupResponse] = union('signupResponse', 'success', [apiSuccess, signupError]);
 
+/* Structs */
 export type User = Reify<typeof serializeUser>;
 export const [serializeUser, deserializeUser] = rec('user', {
   id: str('id'),
